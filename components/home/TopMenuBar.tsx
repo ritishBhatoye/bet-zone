3a0d0d1ff4acfda01993a678e0b81996b8a4180c
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-26 21:29:57 +0530

    Refactor TopMenuBar layout and integrate SearchInputField for improved user experience.

--- components/home/TopMenuBar.tsx
+++ components/home/TopMenuBar.tsx
@@ -2,19 +2,24 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
 import { View, Text } from "react-native";
 
+import SearchInputField from "../atoms/SearchInputField";
+
 const TopMenuBar = () => {
   return (
-    <View className="flex flex-row items-end justify-between px-8 ">
-      <View className="flex flex-col">
-        <Text className="text-lg font-normal">Hello,</Text>
-        <Text className="text-lg font-semibold">RAHUL BHATOYE</Text>
+    <View className="px-8 gap-3">
+      <View className="flex flex-row items-end justify-between ">
+        <View className="flex flex-col">
+          <Text className="text-lg font-normal">Hello,</Text>
+          <Text className="text-lg font-semibold">RAHUL BHATOYE</Text>
+        </View>
+        <Ionicons
+          name="add-outline"
+          color={"white"}
+          size={20}
+          className="p-2 rounded-lg bg-orange-500"
+        />
       </View>
-      <Ionicons
-        name="add-outline"
-        color={"white"}
-        size={20}
-        className="p-2 rounded-lg bg-orange-500"
-      />
+      <SearchInputField />
     </View>
   );
 };
