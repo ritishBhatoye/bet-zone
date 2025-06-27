ca51ebeba0c7666e115397e2a7577dc9711b040f
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-27 22:03:05 +0530

    Update EventItemType interface to include a description property for better event item details.

---
+++ constants/home.ts
@@ -0,0 +1,36 @@
+import { Colors } from "./Colors";
+
+export const sliderDummy = [
+  {
+    id: 1,
+    matchType: "International T20",
+    event: "India vs Australia",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg",
+    color: Colors.light.tint,
+  },
+  {
+    id: 2,
+    matchType: "Champions League",
+    event: "Real Madrid vs Liverpool",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/56/Real_Madrid_CF.svg",
+    color: Colors.dark.tint,
+  },
+  {
+    id: 3,
+    matchType: "NBA Finals",
+    event: "Lakers vs Heat",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/3/3c/Los_Angeles_Lakers_logo.svg",
+    color: "#FFB476",
+  },
+  {
+    id: 4,
+    matchType: "Grand Slam Final",
+    event: "Nadal vs Federer",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/81/Rafael_Nadal_2011.svg",
+    color: "#0a7ea4",
+  },
+];
