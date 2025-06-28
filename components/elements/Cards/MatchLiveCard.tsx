de53c00aaa02d6262c5d0fa3e6fabb4c6c8292b0
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-28 13:19:35 +0530

    Refactor MatchLiveCard component to enhance layout and presentation: adjust structure for team logos and scores, improve alignment with flexbox, and update text styling for better readability.

--- components/elements/Cards/MatchLiveCard.tsx
+++ components/elements/Cards/MatchLiveCard.tsx
@@ -19,15 +19,32 @@ const MatchLiveCard = ({ liveItem }: props) => {
       style={{ borderRadius: 24 }}
     >
       <View className="p-5">
-        <View className="">
-          <View>
+        <View className="flex-row justify-evenly gap-3 px-2">
+          <View className="flex-col items-center">
+            <Image source={liveItem.team.logo} />
+            <Text className="text-lg text-black font-medium">
+              {liveItem.team.formar}
+            </Text>
+          </View>
+          <View className="flex-col items-center">
+            <Text className="text-xs font-normal text-gray-400">
+              {liveItem.leagueType}
+            </Text>
+            <Text className="text-black text-2xl font-medium">
+              {liveItem.score}
+            </Text>
+            <Text className="text-xs font-normal text-gray-400">
+              {liveItem.duration}
+            </Text>
+          </View>
+          <View className="flex-col items-center">
             <Image source={liveItem.team.logo} />
             <Text className="text-lg text-black font-medium">
               {liveItem.team.formar}
             </Text>
           </View>
         </View>
-        <View className="gap-3 justify-around px-2">
+        <View className="gap-3 flex-row justify-around px-2">
           {liveItem.winProbability.map((win: any) => (
             <View key={win.id} className="p-5 bg-gray-300 ">
               <Text className="text-black font-semibold text-base">
